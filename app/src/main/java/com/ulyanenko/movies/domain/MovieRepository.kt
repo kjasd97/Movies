8ab86264b7ec90ccabff7d9c2d0251a289625ad8
Roman Ulyanenko <ulyanenkorr@gmail.com>
2023-05-07 14:46:45 +0300

    add Completable in remove/add methods and change them into another thread

--- app/src/main/java/com/ulyanenko/movies/domain/MovieRepository.kt
+++ app/src/main/java/com/ulyanenko/movies/domain/MovieRepository.kt
@@ -2,15 +2,16 @@ package com.ulyanenko.movies.domain
 
 import androidx.lifecycle.LiveData
 import com.ulyanenko.movies.data.Movie
+import io.reactivex.rxjava3.core.Completable
 
 interface MovieRepository {
 
-    fun addMovie(movie:Movie)
+    fun addMovie(movie:Movie):Completable
 
     fun getMovies ():LiveData<List<Movie>>
 
     fun getMovie(id:Int):LiveData<Movie>
 
-    fun deleteMovie(id:Int)
+    fun deleteMovie(id:Int):Completable
 
 }
--- app/src/main/java/com/ulyanenko/movies/domain/RemoveMovieUseCase.kt
+++ app/src/main/java/com/ulyanenko/movies/domain/RemoveMovieUseCase.kt
@@ -1,9 +1,11 @@
 package com.ulyanenko.movies.domain
 
+import io.reactivex.rxjava3.core.Completable
+
 class RemoveMovieUseCase(private val movieRepository: MovieRepository) {
 
-    fun deleteMovie(id:Int){
-        movieRepository.deleteMovie(id)
+    fun deleteMovie(id:Int):Completable{
+       return movieRepository.deleteMovie(id)
     }
 
 }
